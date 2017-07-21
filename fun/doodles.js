e535b238ca9eeeda024d21345bbce91423f65324
Grace <grace.yin@ibm.com>
2017-07-21 16:59:30 -0400

    image record text file

--- fun/doodles.js
+++ fun/doodles.js
@@ -1,23 +1,34 @@
-var dir = "doodles/";
+var record = "https://graceyin1218.github.io/fun/doodles/_record.txt";
+
+var dir = "https://graceyin1218.github.io/fun/doodles/"
 
 $(document).ready(function() {
-  $.ajax({
-    url:dir, 
-    success: function(data) {
+//  $.ajax({
+//    url:dir, 
+//    success: function(data) {
+
+      $.get(record, function(data, status) {
+        var images = data.split(/\n/);
+        for (image of images) {
+          $("body").append("<img src='" + dir + image + "'>");
+        }
+      });
+
+    /*
       $(data).find("a").attr("href", function(i, val) {
         console.log(val);
         if (val.match(/\.(jpe?g|png|gif$/) ) {
           $("body").append("<img src='" + folder + val + "'>");
         }
       });
-    
+    */
     /*$(data).find("a:contains(" + fileextension + ")").each(function() {
       var filename = this.href.replace(window.location, "").replace("http://", "");
       $("body").append("<img src='" + dir + filename + "'>");
     });
     */
 
-    }
-  });
+//    }
+//  });
 });
 
